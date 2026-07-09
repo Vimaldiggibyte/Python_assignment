3397cf9a0273978f105c40931899ac9be2ffb6e4
VimalRajSt <vimalrajtemp@gmail.com>
2026-07-09 15:49:31 +0530

    Add Calendar file

--- src/Calendar/util.py
+++ src/Calendar/util.py
@@ -1,6 +1,5 @@
 import datetime
-
-
+#datetime library
 def calendar_module(month: int, day: int, year: int) -> str:
     date = datetime.date(year, month, day)
     return date.strftime("%A").upper()
