334c4fbdcd56dad09402b9aa491286f01d393531
VimalRajSt <vimalrajtemp@gmail.com>
2026-07-10 14:39:35 +0530

    Add Validate Email program

--- src/Validate_Email/util.py
+++ src/Validate_Email/util.py
@@ -6,5 +6,6 @@ def is_valid(email: str) -> bool:
     return bool(re.match(pattern, email))
 
 
+
 def filter_emails(emails: list[str]) -> list[str]:
     return sorted(list(filter(is_valid, emails)))
